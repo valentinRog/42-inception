b85473709065679f8439d679bd645057ac2034db
valentinRog <valentin.rogister@gmail.com>
2022-11-27 20:38:33 +0100

    adminer in his own container

---
+++ srcs/requirements/bonus/adminer/tools/configure.sh
@@ -0,0 +1,3 @@
+wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O adminer.php
+wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O adminer.css
+mv adminer.php index.php
